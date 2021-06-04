76a86a0a68924383e22ce8fb24044cda9d06f2d1
tss18 <teagzking@gmail.com>
2021-06-04 18:03:03 +1200

    COMPX341-21A1V3

--- assets/src/_tests_/BSTest.test.tsx
+++ assets/src/_tests_/BSTest.test.tsx
@@ -11,5 +11,13 @@ it("Renders Without Crashing",() => {
     shallow(<Bestseller/>);
 });
 
-
+//describes what the test is set to do - it tests the title of the best sellers in h3
+describe("Bestseller Title Test", () => {
+  test("BestSellers Render Heading Should Be: 'Top 20 Best Sellers'", () => {
+      const header = <h3>Top 20 Best Sellers</h3> //this sets the heading to what I want it to be
+      const wrapping = shallow(<Bestseller />); //this renders the page for bestsellers
+      
+      expect(wrapping.contains(header)).toEqual(true); //does a check to see if the heading matches
+  })
+})
 
